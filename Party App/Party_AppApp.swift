8abcc4e7e663a70a71b836a950c0722dbd05fcd9
Stanley Sim <simyseng@gmail.com>
2024-01-20 15:54:51 +0800

    Initial Commit

---
+++ Party App/Party_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Party_AppApp.swift
+//  Party App
+//
+//  Created by Stanley Sim on 20/1/24.
+//
+
+import SwiftUI
+
+@main
+struct Party_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
